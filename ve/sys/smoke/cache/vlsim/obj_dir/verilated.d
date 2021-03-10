a87d4a27b44cad621d8dc55e4809c1b549180bdc
Matthew Ballance <matt.ballance@gmail.com>
2021-03-09 17:13:44 -0800

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- ve/sys/smoke/cache/vlsim/obj_dir/verilated.d
+++
@@ -1,9 +0,0 @@
-verilated.o: \
- /project/tools/verilator/4.102/share/verilator/include/verilated.cpp \
- /project/tools/verilator/4.102/share/verilator/include/verilatedos.h \
- /project/tools/verilator/4.102/share/verilator/include/verilated_imp.h \
- /project/tools/verilator/4.102/share/verilator/include/verilated.h \
- /project/tools/verilator/4.102/share/verilator/include/verilated_heavy.h \
- /project/tools/verilator/4.102/share/verilator/include/verilated_syms.h \
- /project/tools/verilator/4.102/share/verilator/include/verilated_sym_props.h \
- /project/tools/verilator/4.102/share/verilator/include/verilated_config.h
--- ve/sys/smoke/obj_dir/Vvltest_cls_pkg.cpp
+++
@@ -1,8 +0,0 @@
-// Verilated -*- C++ -*-
-// DESCRIPTION: Verilator output: Design implementation internals
-// See Vvltest.h for the primary calling header
-
-#include "Vvltest_cls_pkg.h"
-#include "Vvltest__Syms.h"
-
-//==========
