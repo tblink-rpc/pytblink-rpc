12c1d92275a708f90eb6fc7e4295a92fcfdae685
Matthew Ballance <matt.ballance@gmail.com>
2022-01-02 10:14:45 -0800

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/tblink_rpc/rt/endpoint_sequencer.py
+++ src/tblink_rpc/rt/endpoint_sequencer.py
@@ -16,8 +16,10 @@ from tblink_rpc_core.event_type_e import EventTypeE
 
 
 class State(Enum):
+    AwaitInit = auto()
+    AwaitBuilt = auto()
+    RunPropagate = auto()
     ProcessMessages = auto()
-    
 
 class EndpointSequencer(object):
     
@@ -32,8 +34,16 @@ class EndpointSequencer(object):
         self.outstanding_nb_reqs = 0
         self.inbound_events = 0
         self.terminated = False
+        self.cls_i = None
+        self.async_state = State.AwaitInit
 
     def run(self):
+        if self.is_async:
+            self._async_run()
+        else:
+            self._sync_run()
+            
+    def _sync_run(self):
         code = 0
         while True:
             print("--> is_init", flush=True)
@@ -51,7 +61,86 @@ class EndpointSequencer(object):
 
         if code != 1:
             raise Exception("Initialization sequence failed")
+        
+        self._create_tb()
+        
+        while True:
+            code = self.ep.is_build_complete()
+            if code == -1:
+                raise Exception("TbLink Error: is_build_complete failed")
+            elif code == 0:
+                if self.ep.process_one_message() == -1:
+                    raise Exception("TbLink Error: process_one_message error during is_build_complete")
+            else:
+                break
+            
+        try:
+            self.cls_i._do_connect(self.ep)
+        except Exception as e:
+            print("Exception during connect: %s" % str(e), flush=True)
+            traceback.print_exc()
+            raise e
+                    
+        if self.ep.connect_complete() == -1:
+            raise Exception("TbLink Error: connect_complete failed")
+        
+        while True:
+            code = self.ep.is_connect_complete()
+            if code == -1:
+                raise Exception("TbLink Error: is_connect_complete failed")
+            elif code == 0:
+                if self.ep.process_one_message() == -1:
+                    raise Exception("TbLink Error: process_one_message error during is_connect_complete")
+            else:
+                break
+            
+        try:
+            self.cls_i._do_start()
+        except Exception as e:
+            print("Exception during start: %s" % str(e), flush=True)
+            traceback.print_exc()
+            raise e
 
+        while not self.terminated:
+            # Spin the loop while we have outstanding non-blocking calls
+            # or while we're still receiving outbound time-centric requests
+            print("--> Process Python activity", flush=True)
+            while True:
+                self.new_time_req = False
+
+                self.propagate_events()                
+
+                # print(" -- outstanding_nb_reqs=%d new_time_req=%d" % (
+                #     self.outstanding_nb_reqs, self.new_time_req), flush=True)
+                # if self.outstanding_nb_reqs == 0 and not self.new_time_req:
+                #     print("  -- No new events", flush=True)
+                #     break
+                # elif self.outstanding_nb_reqs > 0:
+                #     if self.ep.process_one_message() == -1:
+                #         self.terminated = True
+                #         raise Exception("Unexpected disconnect")
+            print("<-- Process Python activity", flush=True)
+            
+            if not self.cls_i._have_objections():
+                print("Done -- no objections", flush=True)
+                break
+            else:
+                print("--> Release peer", flush=True)
+                self.ep.update_comm_mode(comm_mode_e.Automatic, comm_state_e.Released)
+                print("<-- Release peer", flush=True)
+                
+                print("--> Await events", flush=True)
+                self.inbound_events = 0
+                while self.inbound_events == 0:
+                    print("--> process_one_message", flush=True)
+                    if self.ep.process_one_message() == -1:
+                        raise Exception("Unexpected disconnect")
+                    print("<-- process_one_message", flush=True)
+                print("<-- Await events: %d" % self.inbound_events, flush=True)
+        
+        pass        
+        
+    def _create_tb(self):
         clsname = None        
         for a in self.ep.args():
             print("Arg: %s" % str(a))
@@ -76,7 +165,7 @@ class EndpointSequencer(object):
         print("cls: %s" % str(cls))
         
         try:
-            cls_i = cls("root", None)
+            self.cls_i = cls("root", None)
         except Exception as e:
             print("Execption: %s" % str(e), flush=True)
             raise e
@@ -86,98 +175,94 @@ class EndpointSequencer(object):
             
         # Run the build phase
         try:
-            cls_i._do_build(self.backend, self.ep)
+            self.cls_i._do_build(self.backend, self.ep)
         except Exception as e:
             print("Exception during build: %s" % str(e))
             raise e
         
         if self.ep.build_complete() == -1:
             raise Exception("TbLink Error: build_complete failed")
-        
-        while True:
-            code = self.ep.is_build_complete()
-            if code == -1:
-                raise Exception("TbLink Error: is_build_complete failed")
-            elif code == 0:
-                if self.ep.process_one_message() == -1:
-                    raise Exception("TbLink Error: process_one_message error during is_build_complete")
-            else:
-                break
-            
-        try:
-            cls_i._do_connect(self.ep)
-        except Exception as e:
-            print("Exception during connect: %s" % str(e), flush=True)
-            traceback.print_exc()
-            raise e
-                    
-        if self.ep.connect_complete() == -1:
-            raise Exception("TbLink Error: connect_complete failed")
-        
-        while True:
-            code = self.ep.is_connect_complete()
-            if code == -1:
-                raise Exception("TbLink Error: is_connect_complete failed")
-            elif code == 0:
-                if self.ep.process_one_message() == -1:
-                    raise Exception("TbLink Error: process_one_message error during is_connect_complete")
-            else:
-                break
-            
-        try:
-            cls_i._do_start()
-        except Exception as e:
-            print("Exception during start: %s" % str(e), flush=True)
-            traceback.print_exc()
-            raise e
-
+    
+    def propagate_events(self):
         loop = asyncio.get_event_loop()
         def _end_reschedule():
             nonlocal loop
             print("-- _end_reschedule")
-            loop.stop()
-
-        while not self.terminated:
-            # Spin the loop while we have outstanding non-blocking calls
-            # or while we're still receiving outbound time-centric requests
-            print("--> Process Python activity", flush=True)
-            while True:
-                self.new_time_req = False
-                
-                loop.call_soon(_end_reschedule)
-                loop.run_forever()
-
-                print(" -- outstanding_nb_reqs=%d new_time_req=%d" % (
-                    self.outstanding_nb_reqs, self.new_time_req), flush=True)
-                if self.outstanding_nb_reqs == 0 and not self.new_time_req:
-                    print("  -- No new events", flush=True)
-                    break
-                elif self.outstanding_nb_reqs > 0:
-                    if self.ep.process_one_message() == -1:
-                        self.terminated = True
-                        raise Exception("Unexpected disconnect")
-            print("<-- Process Python activity", flush=True)
+            loop.stop()        
             
-            if not cls_i._have_objections():
-                print("Done -- no objections", flush=True)
-                break
-            else:
-                print("--> Release peer", flush=True)
-                self.ep.update_comm_mode(comm_mode_e.Automatic, comm_state_e.Released)
-                print("<-- Release peer", flush=True)
-                
-                print("--> Await events", flush=True)
-                self.inbound_events = 0
-                while self.inbound_events == 0:
-                    print("--> process_one_message", flush=True)
-                    if self.ep.process_one_message() == -1:
-                        raise Exception("Unexpected disconnect")
-                    print("<-- process_one_message", flush=True)
-                print("<-- Await events: %d" % self.inbound_events, flush=True)
-        
-        pass
+        loop.call_soon(_end_reschedule)
+        loop.run_forever()
+       
+        print(" -- outstanding_nb_reqs=%d new_time_req=%d" % (
+            self.outstanding_nb_reqs, self.new_time_req), flush=True)
+        if self.outstanding_nb_reqs == 0 and not self.new_time_req:
+            print("  -- No new events", flush=True)
+            return True
+        elif self.outstanding_nb_reqs > 0:
+            if self.ep.process_one_message() == -1:
+                self.terminated = True
+                raise Exception("Unexpected disconnect")        
+        return False
     
-    def update_state(self):
+    def _async_run(self):
+        """"""
+        print("--> _async_run", flush=True)
+        if self.async_state == State.AwaitInit:
+            if self.ep.is_init() == 1:
+                # Creates the TB class and calls build_complete
+                self._create_tb()
+                self.async_state = State.AwaitBuilt
+                
+        if self.async_state == State.AwaitBuilt:
+            code = self.ep.is_build_complete()
+            if code == -1:
+                raise Exception("TbLink Error: is_build_complete failed")
+            elif code == 1:
+                
+                try:
+                    self.cls_i._do_connect(self.ep)
+                except Exception as e:
+                    print("Exception during connect: %s" % str(e), flush=True)
+                    traceback.print_exc()
+                    raise e
+                    
+                if self.ep.connect_complete() == -1:
+                    raise Exception("TbLink Error: connect_complete failed")
+                self.async_state = State.AwaitConnected
+                
+        if self.async_state == State.AwaitConnected:
+            code = self.ep.is_connect_complete()
+            
+            if code == -1:
+                raise Exception("TbLink Error: is_connect_complete failed")
+            elif code == 1:
+                try:
+                    self.cls_i._do_start()
+                except Exception as e:
+                    print("Exception during start: %s" % str(e), flush=True)
+                    traceback.print_exc()
+                    raise e
+                
+                self.async_state = State.RunPropagate
+                
+        if self.async_state == State.RunPropagate:
+            loop = asyncio.get_event_loop()
+            
+            def _end_reschedule():
+                nonlocal loop
+                print("-- _end_reschedule")
+                loop.stop()        
+            
+            loop.call_soon(_end_reschedule)
+            loop.run_forever()            
+            
+            if not self.cls_i._have_objections():
+                print("No objections... Time to halt")
+            else:
+                print("Release peer")
+                self.ep.update_comm_mode(comm_mode_e.Automatic, comm_state_e.Released)
+                        
+        print("<-- _async_run", flush=True)
         pass
     
     def event(self, ev):
@@ -197,5 +282,8 @@ class EndpointSequencer(object):
             self.inbound_events += 1
         elif kind == EventTypeE.InInvokeReqNB:
             self.inbound_events += 1
+            
+        if self.is_async:
+            self._async_run()
         pass
     
